c7d3cf8cae3a4743ea09ad35ec10d6ef22e579f1
add-gustavo <gustavo20martinss@gmail.com>
2024-11-20 00:08:29 -0300

    updates gerais

---
+++ cadastromunicipal/src/main/java/com/br/cmpcd/ControleJsp.java
@@ -0,0 +1,65 @@
+package com.br.cmpcd;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ControleJsp extends HttpServlet {
+
+    public ControleJsp() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        requisicao(request, response);
+    };
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    };
+
+    private void requisicao(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String acao = request.getParameter("acao");
+        if (acao == null || acao.isEmpty()) {
+            response.sendRedirect("erro.html"); // ou redirecionar para uma página de erro ou login
+            return;
+        }
+
+        try {
+            switch (acao) {
+                case "login":
+                    login(request, response);
+                    break;
+                case "cadastro":
+                    UsuarioPCD(request, response);
+                    break;
+                default:
+                    break;
+            }
+        } catch (ServletException e) {
+        }
+
+    };
+
+    private void login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/pagina/login.jsp");
+        dispatcher.forward(request, response);
+
+    };
+
+    private void UsuarioPCD(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("cadastro.html");
+        dispatcher.forward(request, response);
+
+    };
+
+}
